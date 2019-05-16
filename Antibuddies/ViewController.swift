8e9e35abc5bb6c5f3a98f39dbcb60b1e7d4a1fa7
Mackenzie <mackenziehampel@weber.edu>
2019-05-16 16:30:56 -0600

    Initial Commit

---
+++ Antibuddies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Antibuddies
+//
+//  Created by Mackenzie Hampel on 5/16/19.
+//  Copyright © 2019 WeberStateUniversity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
