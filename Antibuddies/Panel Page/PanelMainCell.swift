58ca3370f8954a0d20b191a17a53dc41e7bf9e7a
Mackenzie <mackenziehampel@weber.edu>
2019-07-08 17:30:30 -0600

    saving progress

--- Antibuddies/Panel Page/PanelMainCell.swift
+++ Antibuddies/Panel Page/PanelMainCell.swift
@@ -8,20 +8,146 @@
 
 import UIKit
 
+protocol buttonTapped {
+    func buttonTapped(cell: PanelMainCell, buttonColumn: String)
+}
+
 class PanelMainCell: UITableViewCell {
 
     @IBOutlet weak var greyNumberLbl: UILabel!
-    
+    @IBOutlet weak var D: UIButton!
+    @IBOutlet weak var bigC: UIButton!
+    @IBOutlet weak var littleC: UIButton!
+    @IBOutlet weak var bigE: UIButton!
+    @IBOutlet weak var littleE: UIButton!
+    @IBOutlet weak var f: UIButton!
+    @IBOutlet weak var V: UIButton!
+    @IBOutlet weak var Cw: UIButton!
+    @IBOutlet weak var BigK: UIButton!
+    @IBOutlet weak var littleK: UIButton!
+    @IBOutlet weak var Kpa: UIButton!
+    @IBOutlet weak var Kpb: UIButton!
+    @IBOutlet weak var Jsa: UIButton!
+    @IBOutlet weak var JSb: UIButton!
+    @IBOutlet weak var Fya: UIButton!
+    @IBOutlet weak var Fyb: UIButton!
+    @IBOutlet weak var Jka: UIButton!
+    @IBOutlet weak var Jkb: UIButton!
+    @IBOutlet weak var Lea: UIButton!
+    @IBOutlet weak var Leb: UIButton!
+    @IBOutlet weak var P1: UIButton!
+    @IBOutlet weak var M: UIButton!
+    @IBOutlet weak var N: UIButton!
+    @IBOutlet weak var S: UIButton!
+    @IBOutlet weak var littleS: UIButton!
+    @IBOutlet weak var Lua: UIButton!
+    @IBOutlet weak var Lub: UIButton!
+    @IBOutlet weak var Xga: UIButton!
+    @IBOutlet weak var IS: UIButton!
+    @IBOutlet weak var thirtySeven: UIButton!
+    @IBOutlet weak var AHG: UIButton!
+    @IBOutlet weak var CC: UIButton!
+    var panelData = [UIButton]()
+    var delegate: buttonTapped!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        panelData = [D, bigC, littleC, bigE, littleE, f, V, Cw, BigK, littleK, Kpa, Kpb,Jsa,JSb,Fya,Fyb,Jka, Jkb,Lea,Leb,P1,M,N,S,littleS,Lua,Lub,Xga,IS,thirtySeven,AHG,CC]
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        //get data from server and load.
         // Configure the view for the selected state
+        
+    }
+    
+    @IBAction func didSelectD(_ sender: Any) {
+       // D.titleLabel?.text = "/"
+        self.delegate.buttonTapped(cell: self, buttonColumn: "D")
+        
+    }
+    
+    @IBAction func didSelectBigC(_ sender: Any) {
+        //D.titleLabel?.text = "/"
+        self.delegate.buttonTapped(cell: self, buttonColumn: "BigC")
+    }
+    
+    @IBAction func didSelectLittleC(_ sender: Any) {
+       // D.titleLabel?.text = "/"
+        self.delegate.buttonTapped(cell: self, buttonColumn: "LittleC")
+    }
+    
+    @IBAction func didSelectBigE(_ sender: Any) {
+        self.delegate.buttonTapped(cell: self, buttonColumn: "BigE")
+    }
+    @IBAction func didSelectLittleE(_ sender: Any) {
+        self.delegate.buttonTapped(cell: self, buttonColumn: "LittleE")
+    }
+    
+    @IBAction func didSelectF(_ sender: Any) {
+    }
+    @IBAction func didSelectV(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectCW(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectBigK(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectLittleK(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectKPA(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectKPB(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectJSA(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectJSB(_ sender: Any) {
+    }
+    @IBAction func didSelectFYA(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectFYB(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectJKA(_ sender: Any) {
+    }
+    @IBAction func didSelectJKB(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectLEA(_ sender: Any) {
+    }
+    @IBAction func didSelectLEB(_ sender: Any) {
+    }
+    @IBAction func didSelectP1(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectM(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectN(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectBigS(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectLittleS(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectLUA(_ sender: Any) {
+    }
+    
+    @IBAction func didSelectLUB(_ sender: Any) {
+    }
+    @IBAction func didSelectXGA(_ sender: Any) {
     }
     
 }
--- Antibuddies/Panel Page/PanelViewController.swift
+++ Antibuddies/Panel Page/PanelViewController.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 
 
-class PanelViewController: UIViewController,  UITableViewDelegate, UITableViewDataSource {
+class PanelViewController: UIViewController,  UITableViewDelegate, UITableViewDataSource, buttonTapped {
    
   @IBOutlet weak var tableView: UITableView!
     
@@ -19,24 +19,56 @@ class PanelViewController: UIViewController,  UITableViewDelegate, UITableViewDa
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.tableView.register(UINib(nibName: "PanelCell", bundle: nil), forCellReuseIdentifier: "PanelCell")
-        
+        self.tableView.register(UINib(nibName: "PanelMainCell", bundle: nil), forCellReuseIdentifier: "PanelMainCell")
+        tableView.backgroundView?.backgroundColor = .clear
+       // tableView.allowsSelection = false;
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return 1
-       
+        return 12
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         if indexPath.row == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "PanelCell", for: indexPath) as! PanelCell
+           
             return cell
         } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "PanelCell", for: indexPath) as! PanelCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PanelMainCell", for: indexPath) as! PanelMainCell
+            cell.greyNumberLbl.text = indexPath.row.description
+            cell.delegate = self
             return cell
         }
         
         
     }
     
+    func buttonTapped(cell: PanelMainCell, buttonColumn: String) {
+        //send in button
+        //button.titleLabel?.text = "/"
+        switch buttonColumn {
+        case "D":
+            //programmatically add
+            cell.D.titleLabel!.text = "/"
+            break
+        case "BigC":
+            cell.bigC.titleLabel?.text = "/"
+            break
+        case "LittleC":
+            cell.littleC.titleLabel?.text = "/"
+            break
+        case "LittleE":
+            cell.littleE.titleLabel?.text = "/"
+            break
+        case "BigE":
+            cell.bigE.titleLabel?.text = "/"
+            break
+        default:
+            break
+        }
+        print("made it")
+    }
 }
